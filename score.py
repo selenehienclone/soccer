b1c9e2cb75b076cc1164b9f40f8ee75576eea521
selenehienclone <vaporflyb@gmail.com>
2023-12-01 10:19:48 -0500

    smaller crop

--- score.py
+++ score.py
@@ -14,14 +14,19 @@ def crop_image(image_path, save_path_result):
     height, width, _ = img.shape
 
     # Crop the top 20%
-    top_crop = int(height * 0.2)
+    top_crop = int(height * 0.10)
     top_cropped_img = img[:top_crop, :]
 
     # Get the middle 30% horizontally of the top 20% and save as result image
-    middle_width_start = int(width * 0.35)
-    middle_width_end = int(width * 0.65)
-    result_img = top_cropped_img[:, middle_width_start:middle_width_end]
-    cv2.imwrite(save_path_result, result_img)
+
+    p1_score_left = int(width * 0.40)
+    p1_score_right = int(width * 0.43)
+    p2_score_left = int(width * 0.55)
+    p2_score_right = int(width * 0.58)
+    p1_score = top_cropped_img[:, p1_score_left:p1_score_right]
+    p2_score = top_cropped_img[:, p2_score_left:p2_score_right]
+    cv2.imwrite('p1_score.png', p1_score)
+    cv2.imwrite('p2_score.png', p2_score)
 
 # Example usage
 image_path = 'field2.png'
@@ -29,8 +34,14 @@ save_path_result = 'result.png'
 
 crop_image(image_path, save_path_result)
 
+import cv2
+import pytesseract
+from PIL import Image
 
-def extract_numbers_from_screenshot(image_path):
+# Set the path to the Tesseract executable (change this to your Tesseract installation path)
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+def identify_number_in_image(image_path):
     # Read the image
     img = cv2.imread(image_path)
 
@@ -38,24 +49,23 @@ def extract_numbers_from_screenshot(image_path):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     # Use Tesseract to extract text
-    text = pytesseract.image_to_string(Image.fromarray(img_rgb))
+    extracted_text = pytesseract.image_to_string(Image.fromarray(img_rgb))
 
-    # Process the extracted text to find two numbers from left to right
-    numbers = process_extracted_text(text)
+    # Process the extracted text to find numbers (assuming it's a single number)
+    numbers = [int(s) for s in extracted_text.split() if s.isdigit()]
 
-    return numbers
-
-def process_extracted_text(text):
-    # Add your logic to process the extracted text and identify two numbers
-    # This example assumes the numbers are the first two integers found in the text
-    numbers = [int(s) for s in text.split() if s.isdigit()][:2]
-    return numbers
+    # If there are numbers, identify the number
+    if numbers:
+        identified_number = numbers[0]
+        return identified_number
+    else:
+        return None
 
 # Example usage
-screenshot_path = 'result.png'
-numbers = extract_numbers_from_screenshot(screenshot_path)
+image_path = 'p1_score.png'
+identified_number = identify_number_in_image(image_path)
 
-if len(numbers) == 2:
-    print(f"The identified numbers from left to right are: {numbers[0]}, {numbers[1]}")
+if identified_number is not None:
+    print(f"The identified number in the image is: {identified_number}")
 else:
-    print("Two numbers not found.")
+    print("No number found in the image.")
