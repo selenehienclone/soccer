3464f031881c9e98cafe14679c416e4021bd8259
selenehienclone <vaporflyb@gmail.com>
2023-12-01 10:26:21 -0500

    each occurences

--- img.py
+++ img.py
@@ -1,54 +1,31 @@
 import cv2
 import numpy as np
 
-def find_multiple_templates(template_path, image_path, num_occurrences):
+def find_template_occurrences(template_path, image_path):
     # Read the template and the image
-    template = cv2.imread(template_path, 0)
-    img = cv2.imread(image_path, 0)
+    template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-    # Set a threshold for the matching result
+    # Perform template matching
+    result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
     threshold = 0.8
+    locations = np.where(result >= threshold)
 
-    # List to store coordinates of matches
-    match_coordinates = []
+    # Get the center coordinates of the occurrences
+    occurrences = []
+    h, w = template.shape
+    for loc in zip(*locations[::-1]):
+        center = (loc[0] + w // 2, loc[1] + h // 2)
+        occurrences.append(center)
 
-    for _ in range(num_occurrences):
-        # Perform template matching
-        result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-
-        # Check if the match exceeds the threshold
-        if max_val >= threshold:
-            # Get the coordinates of the matched area
-            top_left = max_loc
-            h, w = template.shape
-
-            # Save coordinates to the list
-            match_coordinates.append((top_left, (top_left[0] + w, top_left[1] + h)))
-
-            # Draw a rectangle around the matched region
-            bottom_right = (top_left[0] + w, top_left[1] + h)
-            cv2.rectangle(img, top_left, bottom_right, 255, 2)
-
-            # Exclude the matched region from further matching
-            img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]] = 0
-        else:
-            # Break the loop if no more occurrences are found
-            break
-
-    # Display the result
-    cv2.imshow('Matching Result', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    # Return the list of match coordinates
-    return match_coordinates
+    return occurrences
 
 # Example usage
 template_image_path = 'teamA.png'
 main_image_path = 'team1.png'
-matches = find_multiple_templates(template_image_path, main_image_path, num_occurrences=5)
+occurrences = find_template_occurrences(template_image_path, main_image_path)
 
-# Print the match coordinates
-for match in matches:
-    print(f'Match Coordinates: {match}')
+print(f"Number of occurrences found: {len(occurrences)}")
+print("Center coordinates of occurrences:")
+for center in occurrences:
+    print(center)
