686e970e5d561b03ee25b1072861a95ed4960aaf
selenehienclone <vaporflyb@gmail.com>
2023-12-01 09:22:03 -0500

    update pseudocode

---
+++ play.py
@@ -0,0 +1,6 @@
+# Identify when it is my turn
+# Identify what is the score
+# Actions: Identify the five players to move
+# Move one of the players
+# Reward: Score a goal
+# Punishment: Lose a goal
