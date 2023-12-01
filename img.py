f2a67e1046937e73115a1d232fe9789349aee87b
selenehienclone <vaporflyb@gmail.com>
2023-12-01 10:35:19 -0500

    use green circlesA

--- img.py
+++ img.py
@@ -4,10 +4,11 @@ import numpy as np
 def find_template_occurrences(template_path, image_path):
     # Read the template and the image
     template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(image_path, cv2.IMREAD_COLOR)  # Read the image in color
 
-    # Perform template matching
-    result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+    # Perform template matching on the grayscale image
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    result = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
     threshold = 0.8
     locations = np.where(result >= threshold)
 
@@ -20,10 +21,26 @@ def find_template_occurrences(template_path, image_path):
 
     return occurrences
 
+# Function to draw green circles at the centers
+def draw_circles(image, centers):
+    for center in centers:
+        cv2.circle(image, center, 10, (0, 255, 0), -1)  # Draw a green circle
+
 # Example usage
 template_image_path = 'teamA.png'
 main_image_path = 'team1.png'
 occurrences = find_template_occurrences(template_image_path, main_image_path)
+
+# Read the main image in color for drawing circles
+main_image = cv2.imread(main_image_path, cv2.IMREAD_COLOR)
+
+# Draw circles on the main image
+draw_circles(main_image, occurrences)
+
+# Display the result
+cv2.imwrite('result_image.png', main_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 print(f"Number of occurrences found: {len(occurrences)}")
 print("Center coordinates of occurrences:")
